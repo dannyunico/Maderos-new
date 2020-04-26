e7d6800535490bd35350cd8e51a56026753af5e9
Gladibeth <gladibethdelosa@gmail.com>
2020-04-26 16:26:48 -0400

    Add menu fixed total

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ $(function () {
 
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open')
-    // $('.navbar').toggleClass('nav-bg')
+    // $('.img-pd').toggleClass('opac')
     
     
   })
@@ -12,18 +12,33 @@ $(function () {
 
 // Menú fixed
 $(window).scroll(function () {
-  if ($(document).scrollTop() > 70 && ($(window).width() >=0)) {
+  if ($(document).scrollTop() > 70 && ($(window).width() <=991)) {
 
-    $('.navbar').addClass('nav-bg')
+    $('.navbar').addClass('nav-bg2')
     $('#iso').addClass('iso-small')
  
   } else {
-    $('.navbar').removeClass('nav-bg')
+    $('.navbar').removeClass('nav-bg2')
     $('#iso').removeClass('iso-small')
 
   }
 });
 
+// Menú fixed
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >=992)) {
+    $('.navbar-toggler').addClass('navbar-btn')
+  } else {
+    $('.navbar-toggler').removeClass('navbar-btn')
+  }
+});
+
+$('[data-toggle="offcanvas"]').on('click', function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >=992)) {
+    $('.offcanvas-collapse').toggleClass('off-top')
+    $('#img-pd').toggleClass('opac')
+  }  
+})
 
 // menu hambuger
 $(".hamburger").on("click", function () {
@@ -31,22 +46,25 @@ $(".hamburger").on("click", function () {
     $(this).addClass("is-active")
     $('.navbar-fixed-js').addClass('fixed');
     $('.hamburger-inner').addClass('js-hamburger');
-    $('.navbar').addClass('nav-bg2')
+    $('.navbar').addClass('nav-bg')
     // $('html').css('overflow', 'hidden ');
     $('html').addClass('fixed');
-    
+
   } else {
     $(this).removeClass("is-active")
     $('html').removeClass('fixed');
+    $('.navbar').removeClass('nav-bg');
     if ($(document).scrollTop() <= 70 && ($(window).width() <=767)) {
       $('.navbar-fixed-js').removeClass('fixed');
       $('.hamburger-inner').removeClass('js-hamburger');
-      $('.navbar').removeClass('nav-bg2');
+      $('.navbar').removeClass('nav-bg');
       $('html').toggleClass('fixed');
 
     }
   }
 });
+
+
 
 $(function () {
   'use strict'
