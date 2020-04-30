efb7131784a149a1de1530b07b5439a6a6b5551f
Yonnys Quintero <leafar.qr@gmail.com>
2020-04-30 16:10:06 -0400

    formulario de active campign

--- wp-content/themes/Maderos/home.php
+++ wp-content/themes/Maderos/home.php
@@ -63,65 +63,9 @@
 		</div>
 	</section>
 	<!-- form -->
-	<section>
-		<div class="content-form" id="contacto">
-			<div class="form__item">
-				<p>Te llamamos para darte <br> <span>una asesoría completa</span></p>
-			</div>
-			<div class="form__item">
-				<form action="#">
-					<div class="row">
-						<div class="col-md-5 col-lg-4 formulario-banner">
-							<div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-									<input type="text" id="fname" class="form-control "
-										placeholder="Nombre completo*" />
-								</div>
 
-							</div>
-
-							<div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-									<input type="text" id="fname" class="form-control " placeholder="Teléfono*" />
-								</div>
-
-							</div>
-
-							<div class="row form-group">
-								<div class="col-md-12 mb-3 mb-md-0">
-									<input type="text" id="fname" class="form-control " placeholder="Email*" />
-								</div>
-
-							</div>
-
-
-
-						</div>
-						<div class="col-md-6">
-							<div class="row form-group m-0">
-								<div class="col-md-12 p-0">
-									<textarea name="message" id="message" cols="50"  class="form-control barra-text"
-										placeholder="Comentarios"></textarea>
-								</div>
-							</div>
-							<div class="col-md-12 center pl-4 pt-1 pb-1">
-								<input type="checkbox" class="custom-control-input " id="customCheck" name="example1" />
-								<label class="custom-control-label" for="customCheck">Acepto <a target="_blank" href="<?php echo get_template_directory_uri();?>/assets/img/MANUAL_DE_POLITICAS_PROMOTORA.pdf"><span>politicas y
-									privacidad</span></a></label>
-							</div>
-							<div class="row">
-								<div class="col-md-12 text-center">
-									<input type="submit" value="Enviar " class="btn btn-black btn-form " />
-								</div>
-							</div>
-						</div>
-					</div>
-
-
-				</form>
-			</div>
-		</div>
-	</section>
+	<?php get_template_part('partials/contact-banner'); ?>
+	
 
 
 
