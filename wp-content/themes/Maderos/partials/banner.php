5fe41afbd605f9a055d9e425892264b80e5f192f
Yonnys Quintero <leafar.qr@gmail.com>
2020-04-30 22:02:02 -0400

    section, banner, avances de obra, planos, administrable

--- wp-content/themes/Maderos/partials/banner.php
+++ wp-content/themes/Maderos/partials/banner.php
@@ -1,24 +1,17 @@
 <section id="banner" class="banner">
 	<div class="main-banner">
 		<div class="banner-slider">
-			<div class="main-banner__img">
-				<img src="<?php echo get_template_directory_uri();?>/assets/img/slider/banner.png" alt="">
-				<div class="main-banner__text"  data-animation='fadeInUp'>
-					<p>MADEROS <br>DEL <br>RETIRO</p>
-					<hr>
-					<span>Ubicación</span>
-					<p>El Altico, Retiro</p>
-					<span>Área </span>
-					<p>Desde 55 m² hasta 69.9 m²</p>
-					<span>Desde</span>
-					<div class="price">
-						<p>$273.000.000
-						</p>
-						<small>Valor impuestos incluido*</small>
+			<?php $args = array( 'post_type' => 'banner');?>   
+			<?php $loop = new WP_Query( $args ); ?>
+			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				<div class="main-banner__img">
+					<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+					<div class="main-banner__text"  data-animation='fadeInUp'>
+						<?php the_content(); ?>
 					</div>
 				</div>
-			</div>
-			<div class="main-banner__img">
+			<?php endwhile; ?>
+		<!-- 	<div class="main-banner__img">
 				<img src="<?php echo get_template_directory_uri();?>/assets/img/slider/2.png" alt="">
 				<div class="main-banner__text" data-animation='fadeInUp'>
 					<p>MADEROS <br>DEL <br>RETIRO</p>
@@ -51,7 +44,7 @@
 						<small>Valor impuestos incluido*</small>
 					</div>
 				</div>
-			</div>
+			</div> -->
 
 		</div>	
 			<!-- <div class="logo-banner">
--- wp-content/themes/Maderos/partials/planos.php
+++ wp-content/themes/Maderos/partials/planos.php
@@ -2,117 +2,43 @@
 	<div class="content-zone row">
 		<div class="main-plan col-12">
 			<div class="main-planos__content">
-				<div class="main-planos__items">
-					<div class="items-slider">
-						<div class="text-plano">
-							<p class="content-zone__text">PLANOS DEL PROYECTO</p>
-							<hr class="content-zone__text--hr">
+				<?php $args = array( 'post_type' => 'PlanosDelProyecto');?>   
+				<?php $loop = new WP_Query( $args ); ?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+					<div class="main-planos__items">
+						<div class="items-slider">
+							<div class="text-plano">
+								<p class="content-zone__text">PLANOS DEL PROYECTO</p>
+								<hr class="content-zone__text--hr">
+							</div>
+							<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
 						</div>
-						<img src="<?php echo get_template_directory_uri();?>/assets/img/planos/1.png" alt="">
-					</div>
-					<div >
-						<p class="tamaño"><strong class="texto-planos">Apartamento 55,00 m2</strong></p>
+						<div >
+							<p class="tamaño"><strong class="texto-planos"><?php the_title(); ?></strong></p>
 
 
-						<div class="items-planos ">
-							<ul>
-								<li class="wow fadeInUp" data-wow-delay='0.1s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos6.png" alt="">2 habitaciones</li>
-								<li class="wow fadeInUp" data-wow-delay='0.2s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos5.png" alt="">2 baños con ducha</li>
-								<li class="wow fadeInUp" data-wow-delay='0.3s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos2.png" alt="">sala</li>
-								<li class="wow fadeInUp" data-wow-delay='0.4s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">espacio para comedor</li>
-								<li class="wow fadeInUp" data-wow-delay='0.5s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">cocina</li>
-								<li class="wow fadeInUp" data-wow-delay='0.6s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos4.png" alt="">Zona de ropas</li>
-								<li class="wow fadeInUp" data-wow-delay='0.7s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos3.png" alt="">amplio balcón</li>
-								<li class="wow fadeInUp" data-wow-delay='0.8s' ><img  class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> parqueadero en semisótano</li>
-								<li class="wow fadeInUp" data-wow-delay='0.9s'><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> cuarto útil</li>
-							</ul>
+							<div class="items-planos ">
+								<ul>
+									<li class="wow fadeInUp" data-wow-delay="0.1s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos6.png" alt="" /><?php the_field( 'habitaciones' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.2s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos5.png" alt="" /><?php the_field( 'banos_con_ducha' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.3s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos2.png" alt="" /><?php the_field( 'sala' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.4s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="" /><?php the_field( 'espacio_para_comedor' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.5s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="" /><?php the_field( 'cocina' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.6s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos4.png" alt="" /><?php the_field( 'zona_de_ropas' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.7s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos3.png" alt="" /><?php the_field( 'amplio_balcon' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.8s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt="" /> <?php the_field( 'parqueadero_en_semisotano' ); ?></li>
+									<li class="wow fadeInUp" data-wow-delay="0.9s"><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt="" /> <?php the_field( 'cuarto_util' ); ?></li>
+								</ul>
+							</div>
+							<a href="#formulario" class="btn btn-black btn-form btp">OBTENER INORMACIÓN</a>
 						</div>
-						<a href="#formulario" class="btn btn-black btn-form btp">OBTENER INORMACIÓN</a>
 					</div>
-				</div>
-				<div class="main-planos__items">
-					<div class="items-slider">
-						<div class="text-plano">
-							<p class="content-zone__text">PLANOS DEL PROYECTO</p>
-							<hr class="content-zone__text--hr">
 
-						</div>
-						<img src="<?php echo get_template_directory_uri();?>/assets/img/planos/2.png" alt="">
-					</div>
-					<div class="">
-						<p class="tamaño"><strong class="texto-planos">Apartamento 58,00 m2</strong></p>
+				<?php endwhile; ?>
+				
+				
 
-						<div class="items-planos ">
-							<ul>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos6.png" alt="">2 habitaciones</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos5.png" alt="">2 baños con ducha</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos2.png" alt="">sala</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">espacio para comedor</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">cocina</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos4.png" alt="">Zona de ropas</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos3.png" alt="">amplio balcón</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> parqueadero en semisótano</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> cuarto útil</li>
-							</ul>
-						</div>
-						<a href="#formulario" class="btn btn-black btn-form btp">OBTENER INORMACIÓN</a>
-					</div>
-				</div>
-				<div class="main-planos__items">
-					<div class="items-slider">
-						<div class="text-plano">
-							<p class="content-zone__text">PLANOS DEL PROYECTO</p>
-							<hr class="content-zone__text--hr">
-						</div>
-						<img src="<?php echo get_template_directory_uri();?>/assets/img/planos/3.png" alt="">
-					</div>
-					<div class="">
-						<p class="tamaño"><strong class="texto-planos">Apartamento 61,00 m2</strong></p>
-						<div class="items-planos ">
-							<ul>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos6.png" alt="">2 habitaciones</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos5.png" alt="">2 baños con ducha</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos2.png" alt="">sala</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">espacio para comedor</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">cocina</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos4.png" alt="">Zona de ropas</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos3.png" alt="">amplio balcón</li>
-								<li><img  class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> parqueadero en semisótano</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> cuarto útil</li>
-							</ul>
-						</div>
-						<a href="#formulario" class="btn btn-black btn-form btp">OBTENER INORMACIÓN</a>
-					</div>
-				</div>
-
-				<div class="main-planos__items">
-					<div class="items-slider">
-						<div class="text-plano">
-							<p class="content-zone__text">PLANOS DEL PROYECTO</p>
-							<hr class="content-zone__text--hr">
-
-						</div>
-						<img src="<?php echo get_template_directory_uri();?>/assets/img/planos/4.png" alt="">
-					</div>
-					<div class="">
-						<p class="tamaño"><strong class="texto-planos">Apartamento B 69,00 m2</strong></p>
-						<div class="items-planos ">
-							<ul>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos6.png" alt="">3 habitaciones</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos5.png" alt="">2 baños con ducha</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos2.png" alt="">sala</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">espacio para comedor</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos1.png" alt="">cocina</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos4.png" alt="">Zona de ropas</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos3.png" alt="">amplio balcón</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> parqueadero en semisótano</li>
-								<li><img class="iconos-planos" src="<?php echo get_template_directory_uri();?>/assets/img/iconos/icon-planos7.png" alt=""> cuarto útil</li>
-							</ul>
-						</div>
-						<a href="#formulario" class="btn btn-black btn-form btp">OBTENER INORMACIÓN</a>
-					</div>
-				</div>
-
+				
 
 
 			</div>
