8e7a46aedaf3691a135650b1595a14746d06b2fa
Yonnys Quintero <leafar.qr@gmail.com>
2020-05-08 11:56:11 -0400

    optimizar carga de imagenes

--- wp-content/themes/Maderos/header.php
+++ wp-content/themes/Maderos/header.php
@@ -13,8 +13,8 @@
   <meta content='' property='og:description'>
   <meta content='' property='og:image'>
   <link
-    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap"
-    rel="stylesheet">
+  href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap"
+  rel="stylesheet">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/fonts.css">
   <link href="<?php echo get_template_directory_uri();?>/assets/css/slick.css" rel="stylesheet" />
   <link href="<?php echo get_template_directory_uri();?>/assets/css/slick-theme.css" rel="stylesheet" />
@@ -23,30 +23,31 @@
   <link href="<?php echo get_template_directory_uri();?>/assets/css/main.css" rel="stylesheet" />
   <link href="<?php echo get_template_directory_uri();?>/assets/css/responsive.css" rel="stylesheet" />
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/assets/img/favicon-32x32.png">
+  <?php wp_head(); ?>
 </head>
 
 <body>
   <div class="padd__top">
-<header id="home">
-    <nav class="navbar navbar-expand-lg fixed-top ">
-      <div class="main-brand " >
-        <a class="navbar-brand" href="#">
+    <header id="home">
+      <nav class="navbar navbar-expand-lg fixed-top ">
+        <div class="main-brand " >
+          <a class="navbar-brand" href="#">
             <img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="">
           </a>
-        <button class="navbar-toggler p-2 border-0 hamburger hamburger--elastic ml-autos" data-toggle="offcanvas" type="button">
-          <span class="hamburger-box">
-            <span class="hamburger-inner"></span>
-          </span>
-        </button>
-      </div>
-      <div class="navbar-collapse offcanvas-collapse">
+          <button class="navbar-toggler p-2 border-0 hamburger hamburger--elastic ml-autos" data-toggle="offcanvas" type="button">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
+        <div class="navbar-collapse offcanvas-collapse">
           <!-- <img id="img-pd" src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="" /> -->
           <ul class="navbar-nav m-auto">
 
             <li class="nav-item ">
               <a class="nav-link  " href="#detalle-proyecto">detalle de PROYECTO</a>
             </li>
- 
+            
             
             <li class="nav-item">
               <a class="nav-link " href="#planos-proyecto">PLANOS DEL PROYECTO</a>
@@ -54,15 +55,15 @@
             <li class="nav-item">
               <a class="nav-link " href="#galeria">GALERÍA</a>
             </li>
-      
+            
             <li class="nav-item">
               <a class="nav-link " href="#formulario">contacto</a>
             </li>
 
             
           </ul>
-    </nav>
-  </header>
-  
-  
-  
+        </nav>
+      </header>
+      
+      
+      
