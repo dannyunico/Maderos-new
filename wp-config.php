3e3c62b0624933a8fc4aa6b3b2c848f70a206c08
Gladibeth <gladibethdelosa@gmail.com>
2020-05-08 13:06:45 -0400

    Add plugin

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'maderos' );
 define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'di mi clave' );
+define( 'DB_PASSWORD', '123456' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
